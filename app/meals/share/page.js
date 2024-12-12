0f78fc2518787d52170dc26c1737e01264224117
Tariku Bewket <tarikubew@gmail.com>
2024-12-12 16:27:40 +0300

    disable button on submitting and show Submitting message

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,8 +1,7 @@
-'use client';
-
 import ImagePicker from '@/components/meals/image-picker';
 import classes from './page.module.css';
 import { shareMeals } from '@/lib/actions';
+import MealsFormSubmit from '@/components/meals/meals-form-submit';
 
 export default function ShareMealPage() {
   return (
@@ -44,7 +43,7 @@ export default function ShareMealPage() {
           </p>
           <ImagePicker label='Your Image' name='image' />
           <p className={classes.actions}>
-            <button type='submit'>Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
