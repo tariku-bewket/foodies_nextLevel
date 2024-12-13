7a0551d08ef2ea128d9e4f40bb88fc5ab77ceb7a
Tariku Bewket <tarikubew@gmail.com>
2024-12-13 16:38:26 +0300

    add dynamic metadata

--- app/not-found.js
+++ app/not-found.js
@@ -1,8 +1,8 @@
 export default function NotFound() {
   return (
     <main className='not-found'>
-      <h1>Not Found</h1>
+      <h1>Meal not found</h1>
       <p>Unfortunately, the page or resource you requested is not found.</p>
     </main>
-  )
+  );
 }
